a35ed5750d277e90aaf944344c8aa0fa178aed06
Xiaonan Shen <s@sxn.dev>
2021-02-07 10:28:13 +0800

    Expose classes at module level

--- libdyson/__init__.py
+++ libdyson/__init__.py
@@ -1,2 +1,7 @@
 """Dyson Python library."""
+
+# flake8: noqa
+
+from .dyson_360_eye import Dyson360Eye, Dyson360EyePowerMode, Dyson360EyeState
 from .dyson_account import DysonAccount
+from .dyson_pure_cool_link import DysonPureCoolLink, FanMode, FanSpeed

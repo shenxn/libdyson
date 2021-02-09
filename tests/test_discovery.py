abb6780c474ea798a00ee8a0142a071b3dd2c529
Xiaonan Shen <s@sxn.dev>
2021-02-09 14:37:53 +0800

    Improve discovery test

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -1,9 +1,10 @@
 """Test discovery."""
 import socket
+from typing import Optional
 from unittest.mock import MagicMock, patch
 
 import pytest
-from zeroconf import ServiceBrowser, ServiceInfo, ServiceListener, Zeroconf
+from zeroconf import ServiceBrowser, ServiceInfo, Zeroconf
 
 from libdyson import DysonDiscovery
 from libdyson.discovery import TYPE_DYSON_360_EYE, TYPE_DYSON_FAN
@@ -12,38 +13,44 @@ from libdyson.dyson_device import DysonDevice
 from . import CREDENTIAL, HOST, SERIAL
 
 
-def _mocked_service_browser(
-    zeroconf: Zeroconf, type: list, listener: ServiceListener
-) -> ServiceBrowser:
-    service_browser = MagicMock(spec=ServiceBrowser)
-    service_browser.zc = zeroconf
-    service_browser.listener = listener
-    return service_browser
-
-
 @pytest.fixture(autouse=True)
-def _zeroconf():
+def zeroconf_mock() -> MagicMock:
+    """Return zeroconf mock."""
     zeroconf = MagicMock(spec=Zeroconf)
     with patch("libdyson.discovery.Zeroconf", return_value=zeroconf) as mocked:
         yield mocked
 
 
-@pytest.fixture(autouse=True)
-def _service_browser():
+@pytest.fixture()
+def service_browser_mock() -> MagicMock:
+    """Return service browser mock."""
+    service_browser = MagicMock(spec=ServiceBrowser)
+    type(service_browser).zc = MagicMock()
     with patch(
-        "libdyson.discovery.ServiceBrowser", side_effect=_mocked_service_browser
+        "libdyson.discovery.ServiceBrowser",
+        return_value=service_browser,
     ) as mocked:
         yield mocked
 
 
-def test_discovery():
+@pytest.mark.parametrize("zeroconf_instance", [None, MagicMock(spec=Zeroconf)])
+def test_discovery(
+    service_browser_mock: MagicMock,
+    zeroconf_mock: MagicMock,
+    zeroconf_instance: Optional[Zeroconf],
+):
     """Test discovery."""
     discovery = DysonDiscovery()
     device = DysonDevice(SERIAL, CREDENTIAL)
-    discovery.start_discovery()
-    service_browser = discovery._browser
-    zeroconf = service_browser.zc
-    listener = service_browser.listener
+    discovery.start_discovery(zeroconf_instance)
+    service_browser = service_browser_mock.return_value
+    zeroconf = service_browser_mock.call_args.args[0]
+    listener = service_browser_mock.call_args.args[2]
+
+    if zeroconf_instance is None:
+        zeroconf_mock.assert_called_once()
+    else:
+        zeroconf_mock.assert_not_called()
 
     # 360 Eye, discovered before registered
     name = f"360EYE-{SERIAL}.{TYPE_DYSON_360_EYE}"
@@ -69,4 +76,20 @@ def test_discovery():
     # Stop discovery
     discovery.stop_discovery()
     service_browser.cancel.assert_called_once()
-    zeroconf.close.assert_called_once()
+    service_browser.zc.close.assert_called_once()
+
+
+def test_stop_runtime_error():
+    """Test ServiceBrowser.cancel RuntimeError handling."""
+    service_browser = MagicMock(spec=ServiceBrowser)
+    type(service_browser).zc = MagicMock()
+    type(service_browser).cancel = MagicMock(side_effect=RuntimeError)
+    with patch(
+        "libdyson.discovery.ServiceBrowser",
+        return_value=service_browser,
+    ):
+        discovery = DysonDiscovery()
+        discovery.start_discovery()
+        discovery.stop_discovery()
+        service_browser.cancel.assert_called_once()
+        service_browser.zc.close.assert_called_once()

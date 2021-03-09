38e71f27bdc28007191c77252b6ca2873ef65397
Xiaonan Shen <s@sxn.dev>
2021-03-09 23:04:17 +0800

    [Add] Add function to retrieve serial and model

--- calculate_device_credenial.py
+++ calculate_device_credenial.py
@@ -1,8 +1,18 @@
 """Calculate device credential using WiFi information."""
 
-from libdyson import get_mqtt_password_from_wifi_password
+from libdyson import DEVICE_TYPE_NAMES, get_mqtt_info_from_wifi_info
 
-wifi_password = input("WiFi password: ")
+print(
+    "Note that you need to input your Dyson device WiFi information on the sticker"
+    "on user's manual and device body. Do not put your home WiFi information."
+)
 
-mqtt_password = get_mqtt_password_from_wifi_password(wifi_password)
-print("MQTT password: ", mqtt_password)
+wifi_ssid = input("Device WiFi SSID: ")
+wifi_password = input("Device WiFi password: ")
+
+serial, credential, model = get_mqtt_info_from_wifi_info(wifi_ssid, wifi_password)
+print()
+print("Serial:", serial)
+print("Credential:", credential)
+print("Model:", model)
+print("Device Type:", DEVICE_TYPE_NAMES[model])
--- libdyson/utils.py
+++ libdyson/utils.py
@@ -2,7 +2,11 @@
 
 import base64
 import hashlib
+import re
 import time
+from typing import Tuple
+
+from .const import DEVICE_TYPE_360_EYE
 
 
 def mqtt_time():
@@ -16,3 +20,27 @@ def get_mqtt_password_from_wifi_password(wifi_password: str) -> str:
     hash_.update(wifi_password.encode("utf-8"))
     password_hash = base64.b64encode(hash_.digest()).decode("utf-8")
     return password_hash
+
+
+def get_mqtt_info_from_wifi_info(
+    wifi_ssid: str, wifi_password: str
+) -> Tuple[str, str, str]:
+    """Get MQTT information from WiFi information."""
+    result = re.match(r"^[0-9A-Z]{3}-[A-Z]{2}-[0-9A-Z]{8}$", wifi_ssid)
+    if result is not None:
+        serial = wifi_ssid
+        model = DEVICE_TYPE_360_EYE
+    else:
+        result = re.match(
+            r"^DYSON-([0-9A-Z]{3}-[A-Z]{2}-[0-9A-Z]{8})-([0-9]{3})$", wifi_ssid
+        )
+        if result is not None:
+            serial = result.group(1)
+            model = result.group(2)
+        else:
+            print(
+                "Failed to parse SSID. Please report this to https://github.com/shenxn/libdyson/issues/new"
+            )
+
+    password = get_mqtt_password_from_wifi_password(wifi_password)
+    return serial, password, model

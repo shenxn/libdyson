c97f13168e0cff34fcf8084762832b109774c327
David <david@noemail.com>
2021-11-27 16:37:32 +0100

    Add test for new Dyson 360 Eye WiFi SSID

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,6 +11,11 @@ def test_get_mqtt_info_from_wifi_info():
         "wcosm2mJlB57tHnsxp3i8CiSz8H13J4i4p6Jw2SjdW3c+u+AZtG3PNmZ/ldaFS+Auubl5QRC/z3Lk4D3j+DQNw==",
         DEVICE_TYPE_360_EYE,
     )
+    assert get_mqtt_info_from_wifi_info("360EYE-JH1-US-GDA0001A", "z2jks80tmz") == (
+        "JH1-US-GDA0001A",
+        "wcosm2mJlB57tHnsxp3i8CiSz8H13J4i4p6Jw2SjdW3c+u+AZtG3PNmZ/ldaFS+Auubl5QRC/z3Lk4D3j+DQNw==",
+        DEVICE_TYPE_360_EYE,
+    )
     assert get_mqtt_info_from_wifi_info("DYSON-NN3-EU-HWL4729E-475", "hjkj3gjask") == (
         "NN3-EU-HWL4729E",
         "Tp+0uDTy8scsa/PnYOeBVljZJiqzIZG+A0zOe4fiRJWXVKNOI4mpnFX7gDeEl5MEO10KFDbvVn4/4hiE2vpwiw==",

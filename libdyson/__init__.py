6a8ed5c59ecf17d06facb4121ec6752095b7694e
Kevin Lemonnier <873423+Ulrar@users.noreply.github.com>
2022-12-12 12:19:35 +0000

    Adding support for 527K - Dyson Pure Hot+Cool Formaldehyde

--- libdyson/__init__.py
+++ libdyson/__init__.py
@@ -15,6 +15,7 @@ from .const import (
     DEVICE_TYPE_PURE_HOT_COOL_LINK,
     DEVICE_TYPE_PURE_HUMIDIFY_COOL,
     DEVICE_TYPE_PURIFIER_HUMIDIFY_COOL_FORMALDEHYDE,
+    DEVICE_TYPE_PURE_HOT_COOL_FORMALDEHYDE,
 )
 from .const import CleaningMode  # noqa: F401
 from .const import CleaningType  # noqa: F401
@@ -32,7 +33,7 @@ from .dyson_device import DysonDevice
 from .dyson_pure_cool import DysonPureCool
 from .dyson_pure_cool import DysonPureCoolFormaldehyde
 from .dyson_pure_cool_link import DysonPureCoolLink
-from .dyson_pure_hot_cool import DysonPureHotCool
+from .dyson_pure_hot_cool import DysonPureHotCool, DysonPureHotCoolFormaldehyde
 from .dyson_pure_hot_cool_link import DysonPureHotCoolLink
 from .dyson_pure_humidify_cool import DysonPureHumidifyCool, DysonPurifierHumidifyCoolFormaldehyde
 from .utils import get_mqtt_info_from_wifi_info  # noqa: F401
@@ -58,6 +59,8 @@ def get_device(serial: str, credential: str, device_type: str) -> Optional[Dyson
         return DysonPureCoolFormaldehyde(serial, credential, device_type)
     if device_type == DEVICE_TYPE_PURE_HOT_COOL_LINK:
         return DysonPureHotCoolLink(serial, credential, device_type)
+    if device_type == DEVICE_TYPE_PURE_HOT_COOL_FORMALDEHYDE:
+        return DysonPureHotCoolFormaldehyde(serial, credential, device_type)
     if device_type in [
         DEVICE_TYPE_PURE_HOT_COOL,
         DEVICE_TYPE_PURE_HOT_COOL_NEW,
--- libdyson/dyson_pure_hot_cool.py
+++ libdyson/dyson_pure_hot_cool.py
@@ -1,8 +1,17 @@
 """Dyson Pure Hot+Cool device."""
 
+from typing import Optional
+
 from .dyson_device import DysonHeatingDevice
 from .dyson_pure_cool import DysonPureCool
 
-
 class DysonPureHotCool(DysonPureCool, DysonHeatingDevice):
     """Dyson Pure Hot+Cool device."""
+
+class DysonPureHotCoolFormaldehyde(DysonPureHotCool):
+    """Dyson Pure Hot+Cool Formaldehyde device."""
+
+    @property
+    def formaldehyde(self) -> Optional[int]:
+        """Return formaldehyde reading."""
+        return self._get_environmental_field_value("hcho")
